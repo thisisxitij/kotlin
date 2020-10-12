d0242cdf79df72e973281372b97ee2b8d339eaa4
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-10-12 13:29:16 +0300

    ++ Move modality calculation to type resolve stage

--- idea/idea-frontend-fir/idea-fir-low-level-api/src/org/jetbrains/kotlin/idea/fir/low/level/api/FirTransformerProvider.kt
+++ idea/idea-frontend-fir/idea-fir-low-level-api/src/org/jetbrains/kotlin/idea/fir/low/level/api/FirTransformerProvider.kt
@@ -7,7 +7,6 @@ package org.jetbrains.kotlin.idea.fir.low.level.api
 
 import com.google.common.collect.MapMaker
 import org.jetbrains.kotlin.fir.FirSession
-import org.jetbrains.kotlin.fir.FirSessionComponent
 import org.jetbrains.kotlin.fir.declarations.FirResolvePhase
 import org.jetbrains.kotlin.fir.resolve.ScopeSession
 import org.jetbrains.kotlin.fir.resolve.transformers.FirTransformerBasedResolveProcessor
@@ -16,13 +15,13 @@ import java.util.*
 
 class FirTransformerProvider {
     private val scopeSession = MapMaker().weakKeys().makeMap<FirSession, ScopeSession>()
-    private val transformers = MapMaker().weakKeys().makeMap<FirSession, EnumMap<FirResolvePhase, FirTransformerBasedResolveProcessor>>()
+    private val transformers = MapMaker().weakKeys().makeMap<FirSession, EnumMap<FirResolvePhase, FirTransformerBasedResolveProcessor<*>>>()
 
 
-    fun getTransformerForPhase(session: FirSession, phase: FirResolvePhase): FirTransformerBasedResolveProcessor =
+    fun getTransformerForPhase(session: FirSession, phase: FirResolvePhase): FirTransformerBasedResolveProcessor<*> =
         transformers.computeIfAbsent(session) {
             val scopesSession = getScopeSession(session)
-            EnumMap<FirResolvePhase, FirTransformerBasedResolveProcessor>(FirResolvePhase::class.java).apply {
+            EnumMap<FirResolvePhase, FirTransformerBasedResolveProcessor<*>>(FirResolvePhase::class.java).apply {
                 FirResolvePhase.values().forEach { resolvePhase ->
                     if (resolvePhase != FirResolvePhase.RAW_FIR) {
                         put(resolvePhase, resolvePhase.createTransformerBasedProcessorByPhase(session, scopesSession))
